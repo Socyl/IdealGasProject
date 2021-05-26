1aea59e54052be4042d68dd854deb9e9307b784e
DESKTOP-SVECMJF\BC <scorbitt@comcast.net>
2021-05-26 15:05:20 -0600

    Add project files.

---
+++ IdealGasProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IdealGasProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCelsiusToKelvin();
+            
+        }
+        static double CelsiusToKelvin(double celsius)
+        {
+            return celsius + 273.15;
+        }
+
+        static void TestCelsiusToKelvin()
+        {
+            double kelvin = CelsiusToKelvin(0.0);
+            Console.WriteLine($"CelsiusToKelvin => {kelvin}");
+        }
+    }
+}
